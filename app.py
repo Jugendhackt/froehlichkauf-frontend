e25c9b7fe404da3baf12c0101dbe9902bb5a109b
breuxi <breuxi.innovations@gmail.com>
2019-05-26 11:06:55 +0200

    API Connection, Ratings and new Style

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
 from flask import Flask, render_template
 import requests
+import json
+import os
 
 hackdash = "https://hackdash.org/projects/5ce8f714002e766ac0f23602"
+api_url = "https://api.jhcgn19.nwng.eu/getProduct"
 
 application = Flask(__name__, static_url_path='', static_folder="static")
+
+
+def url_for_static(filename):
+    root = application.config.get('STATIC_ROOT', '')
+    return os.path.join(root, filename)
 
 
 @application.route('/')
 def index():
     return render_template("index.html")
+
+
+@application.route('/barcode')
+def no_barcode(barcode):
+    print(barcode)
+    return "Da fehlt ein Barcode!"
+
+
+@application.route('/barcode/<int:barcode>')
+def barcode(barcode):
+
+    headers = {"Content-Type": "application/json"}
+
+    print(barcode)
+    root = application.config.get('STATIC_ROOT', '')
+
+    api_data = requests.post(api_url, data=json.dumps({"code": str(barcode), "origin": "de"}), headers=headers)
+
+    print(api_data.text)
+
+    return render_template("product.html", product=api_data.json(), root=root)
 
 
 # def factors(num):
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,45 @@
-$(document).ready(function () {
+$(document).on('click','#download-button', function(event, test){
+
+    var barcode = $("#barcode").val();
+    console.log(barcode);
+
+    console.log($(location).attr('href'));
+
+    event.preventDefault();
+
+    var url = $(location).attr('origin') + "/barcode/" + barcode;
+    $(location).attr('href', url);
 
 });
+
+
+/*$(document).ready(function () {
+    $('#download-button').on('click', function (e) {
+        console.log("Test!")
+        /*e.preventDefault();
+        $('#download-button').css({'display': 'none'});
+        $('#barcode-input').css({'display': 'block'});*/
+//    });
+
+ //   $("")
+
+/*    Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('#yourElement')    // Or '#yourElement' (optional)
+    },
+    decoder : {
+      readers : ["code_128_reader"]
+    }
+  }, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+  });*/
+
+
+//});*/
